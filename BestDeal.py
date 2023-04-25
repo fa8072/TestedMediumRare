b9e930ebef05db63bde0154d9e3221eaee0722e9
Takumi <thatallql@gmail.com>
2023-04-25 19:43:42 +0300

    add ad(hell)

--- BestDeal.py
+++ BestDeal.py
@@ -9,8 +9,6 @@ app.attributes("-fullscreen", True)
 app.minsize(width=600, height=600)
 
 
-def add_product():
-    pass
 
 def del_product(product):
     def _del_product():
@@ -24,19 +22,42 @@ def success_login(user):
     window.title("Товары")
 
     if user[6] == "admin":
-        title_label = tk.Label(window,
-                               text="Название")
-        title_label.pack()
-
         title = tk.Entry(window)
+        title.insert(0, "Название")
         title.pack()
 
-        price_label = tk.Label(window,
-                               text="Цена")
-        price_label.pack()
+        storage = tk.Entry(window)
+        storage.insert(0, "Хранилище (в MiB)")
+        storage.pack()
+
+        ram = tk.Entry(window)
+        ram.insert(0, "Оперативная память (в MiB)")
+        ram.pack()
+
+        cpu = tk.Entry(window)
+        cpu.insert(0, "Процессор")
+        cpu.pack()
+
+        mhz = tk.Entry(window)
+        mhz.insert(0, "Тактовая частота (в MHz)")
+        mhz.pack()
 
         price = tk.Entry(window)
+        price.insert(0, "Цена")
         price.pack()
+
+        def add_product():
+            with connection.cursor() as cursor:
+                n = cursor.execute("SELECT id FROM products ORDER BY id DESC LIMIT 1").fetchall()
+
+                cursor.execute(f"INSERT INTO products VALUES ({n[0][0] + 1}, "
+                               f"'{title.get()}',"
+                               f"{storage.get()},"
+                               f"{ram.get()},"
+                               f"'{cpu.get()}',"
+                               f"{mhz.get()},"
+                               f"{price.get()})")
+            connection.commit()
 
         add_product_btn = tk.Button(window,
                                     text="Добавить",
@@ -44,18 +65,33 @@ def success_login(user):
 
         add_product_btn.pack()
 
-
-
     with connection.cursor() as cursor:
         products = cursor.execute("SELECT * FROM products").fetchall()
     for product in products:
-        product_label = tk.Label(window,
-                               text= product[1])
-        product_label.pack()
+        product_title_label = tk.Label(window,
+                                 text=product[1])
+        product_title_label.pack()
 
-        product_label = tk.Label(window,
-                               text="-------")
-        product_label.pack()
+        product_storage_label = tk.Label(window,
+                                 text=f"{product[2]} MiB")
+        product_storage_label.pack()
+
+        product_ram_label = tk.Label(window,
+                                 text=f"{product[3]} MiB")
+        product_ram_label.pack()
+
+        product_cpu_label = tk.Label(window,
+                                 text=product[4])
+        product_cpu_label.pack()
+
+        product_mhz_label = tk.Label(window,
+                                 text=f"{product[5]} MHz")
+        product_mhz_label.pack()
+
+        product_price_label = tk.Label(window,
+                                 text=f"{product[6]} Rupee")
+        product_price_label.pack()
+
         if user[6] == "admin":
             del_product_btn = tk.Button(window,
                                         text="Удалить",
@@ -63,6 +99,9 @@ def success_login(user):
 
             del_product_btn.pack()
 
+        separate_label = tk.Label(window,
+                                 text="-------")
+        separate_label.pack()
 
 
 def click_btn_sign_in():
